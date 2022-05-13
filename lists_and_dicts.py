a7df0f767a6c1add900b9dbec58f547ca99f41a1
sayin <luffyp2001@gmail.com>
2022-05-13 14:07:03 -0500

    First commit

---
+++ lists_and_dicts.py
@@ -0,0 +1,29 @@
+def run():
+    my_list = [1,'hello', True, 4.5]
+    my_dict = {'fistname': 'Oscar', 'lastname': 'Piedrahita' }
+
+    super_list =[
+        {'fistname': 'Oscar', 'lastname': 'Piedrahita' },
+        {'fistname': 'Lina', 'lastname': 'Caicedo' },
+        {'fistname': 'Angel', 'lastname': 'Moreno' },
+        {'fistname': 'Laura', 'lastname': 'Romero' },
+        {'fistname': 'Santiago', 'lastname': 'Novoa' }
+    ]
+
+    super_dict ={
+        'natural_nums':[1,2,3,4,5],
+        'integer_nums':[-1,-2,0,1,2],
+        'floating_nums':[1.1,4.5,6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key, '', value)
+
+    for item in super_list:
+        print(item['fistname'], '',  item['lastname'])
+    
+
+
+
+if __name__ == '__main__':
+    run()
